9b76825f26ba1d8330f039953b6dbc88e44d44f5
yuan29346 <yuan29346@163.com>
2017-02-21 10:11:00 +0800

    111111

--- src/com/phei/netty/aio/AsyncTimeServerHandler.java
+++ src/com/phei/netty/aio/AsyncTimeServerHandler.java
@@ -3,6 +3,11 @@
  */
 package com.phei.netty.aio;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.AsynchronousServerSocketChannel;
+import java.util.concurrent.CountDownLatch;
+
 /**
  * @author Administrator
  *
@@ -10,16 +15,38 @@ package com.phei.netty.aio;
 public class AsyncTimeServerHandler implements Runnable {
 
 	private int port;
+	CountDownLatch countDownLatch;
+	AsynchronousServerSocketChannel asynchronousServerSocketChannel;
+	
+	
 	public AsyncTimeServerHandler(int port) {
 		this.port = port;
+		try {
+			this.asynchronousServerSocketChannel = AsynchronousServerSocketChannel.open();
+			asynchronousServerSocketChannel.bind(new InetSocketAddress(port));
+			System.out.println("this server port:"+port);
+			
+		
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
+	
+	
 	/* (non-Javadoc)
 	 * @see java.lang.Runnable#run()
 	 */
 	@Override
 	public void run() {
-	 
-
+	 this.countDownLatch = new CountDownLatch(1);
+	 this.asynchronousServerSocketChannel.accept(this,new AcceptCompletionHandle());
+	 try{
+		 countDownLatch.await();
+	 }catch(Exception e){
+		 
+	 }
 	}
 
 }

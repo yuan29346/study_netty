3e03a7087b86ef5523fd6f26aad5fe9bb99da1d7
yuan29346 <yuan29346@163.com>
2017-02-23 17:32:01 +0800

    222222222222

--- src/com/phei/netty/aio/ReadCompletionHandler.java
+++ src/com/phei/netty/aio/ReadCompletionHandler.java
@@ -30,7 +30,7 @@ public class ReadCompletionHandler implements CompletionHandler<Integer, ByteBuf
 			String req = new String(bytes, "utf-8");
 			System.out.println("the timer server recevie order:" + req);
 			String currentTime = "Query time order".equalsIgnoreCase(req)?new Date().toLocaleString():"BAD ORDER";
-//			TODO
+			doWrite(currentTime);
 		} catch (UnsupportedEncodingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -40,7 +40,26 @@ public class ReadCompletionHandler implements CompletionHandler<Integer, ByteBuf
 	
 	private void doWrite(String currentTime){
 		if(currentTime != null && currentTime.length()>0){
+			byte[] bytes = currentTime.getBytes();
+			ByteBuffer writeBuffer = ByteBuffer.allocate(bytes.length);
+			writeBuffer.put(bytes);
+			writeBuffer.flip();
+			channel.write(writeBuffer,writeBuffer,new CompletionHandler<Integer, ByteBuffer>() {
+
+				@Override
+				public void completed(Integer result, ByteBuffer attachment) {
+					
+					
+				}
+
+				@Override
+				public void failed(Throwable exc, ByteBuffer attachment) {
+					// TODO Auto-generated method stub
+					
+				}
 			
+			
+			});
 		}
 	}
 
